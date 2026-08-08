cb6351d9fe687333b906cee0fd8c05429af8ad11
tuanhuydev <tuanhuydev@gmail.com>
2026-08-08 20:00:32 +0700

    init project

---
+++ nvim/nvim/init.lua
@@ -0,0 +1,11 @@
+vim.loader.enable()
+
+-- General vim config
+vim.o.encoding = 'utf-8'
+vim.o.mouse = 'a'
+vim.o.background = 'dark'
+vim.o.number = true
+vim.o.smartcase = true
+vim.o.ignorecase = true
+vim.o.shiftwidth = 2
+
